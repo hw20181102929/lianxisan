635793f8557b4bc1f4b1998c58cb0ec8e093de14
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-10-23 12:00:59 +0800

    1
    1

--- lianxisan/ViewController.swift
+++ lianxisan/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func run(_ sender: UIButton) {
+        for index in 1...10
+        {
+            if index>0
+            {
+                print("现在有\(11-index)部iphone,卖掉了一部，还剩\(10-index)部")
+            }
+            else
+            {
+                print("全部卖光了")
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
